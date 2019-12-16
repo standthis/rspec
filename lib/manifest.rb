ec8eb7427059e8a8b97f6f6a79d8319a0596f038
standthis <standthis@protonmail.ch>
2019-12-16 18:13:57 +0100

    Implement first and rest

--- lib/manifest.rb
+++ lib/manifest.rb
@@ -1,36 +1,32 @@
 class Manifest 
-  #attr_reader :manifest 
+
   def self.traverse(h, path)
     parts = path.split('/', 3).reject { |e| e.to_s.empty? }
-    self.matchequal(h, parts)
-    if parts.length == 1
+    parts = [parts.first, parts[1..-1].join('/')]
+    puts "#{h} #{parts.inspect}"
+    if parts.length == 1 
       if parts.first.include?('=')
-        return h.first
-      else
-        return h[parts.first]
+        comps = parts.first.split('=')
+        dd = h.find { |pair| pair[comps.first] == comps.last } 
+        puts '1'
+        return dd
       end
-    else  
-      if h.is_a?(Hash)
-        if h.key?(parts.first)
-          traverse(h[parts.first], parts.last)
-        end
-      elsif h.is_a?(Array)
-        if h.include?(parts.first)
-          traverse(h[h.find_index(parts.fist)], parts.last)
-        end
+      return h[parts.first]
+    end  
+    if parts.first.include?('=')
+      comps = parts.first.split('=')
+      dd = h.find { |pair| pair[comps.first] == comps.last } 
+      puts '3'
+      return traverse(dd, parts.last)
+    end
+
+    if h.is_a?(Hash)
+      if h.key?(parts.first)
+        puts '4'
+        return traverse(h[parts.first], parts.last)
       end
     end
+    raise NotImplementedError
   end
-  def self.matchequal(h, component)
-    if component.include?('=')
-      if component.length == 1
-        return h.first
-      else
-        comps = component.split('=')
-        if h.first.has_value?(comps.last)
-          traverse(h.first, component.last)
-        end
-      end
-    end
-  end
+
 end
--- spec/manifest_spec.rb
+++ spec/manifest_spec.rb
@@ -2,24 +2,36 @@ require 'manifest'
 
 RSpec.describe Manifest do
   context "Traversal of manifest yml using BOSH ops path" do
-    it "simple hash" do
-      h = { 'a' => 1 }
-      expect(Manifest.traverse(h,'/a')).to eql(1)
-    end
-    
-    it "another simple hash traversal" do 
-      h = { 'a' => { 'b' => 2 } }
-      expect(Manifest.traverse(h, '/a/b')).to eql(2)
-    end
-
-    it "yash" do
-      h = { 'a' => [ { 'b' => 'value' } ] } 
-      expect(Manifest.traverse(h, '/a/b=value')).to eql({'b' => 'value'})
-    end
-
-    it "yash" do
-      h = { 'a' => [ { 'b' => 'value' } ] } 
-      expect(Manifest.traverse(h, '/a/b=value')).to eql({'b' => 'value'})
+    [
+      #{
+      #  h: { 'a' => 1 },
+      #  path: 'a',
+      #  expected: 1
+      #},
+      #{
+      #  h: { 'a' => { 'b' => 2 } },
+      #  path: '/a/b',
+      #  expected: 2
+      #},
+      #{ 
+      #  h: {'a' => [{'b' => 'value'}]},
+      #  path: '/a/b=value',
+      #  expected: {'b' => 'value'}
+      #},
+      #{
+      #  h: { 'a' => [ { 'b' => 'value', 'c' => { 'd' => 'e'} }, { 'n' => 'value' } ] },
+      #  path: '/a/b=value/c',
+      #  expected: { 'd' => 'e'}
+      #},
+      {
+        h: { 'a' => [ { 'b' => 'value', 'c' => { 'd' => 'e'} }, { 'n' => 'value' } ] },
+        path: '/a/b=value/c/d',
+        expected: 'e'
+      }
+    ].each do |entry|
+      it "works" do
+        expect(Manifest.traverse(entry[:h], entry[:path])).to eql(entry[:expected])
+      end
     end
   end
 end
